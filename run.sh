0b76854dca4a91a3731ebcaf387351d15b481f75
Aloys Augustin <aloys.augustin@polytechnique.org>
2017-11-16 17:17:22 +0100

    Fix run script: get vars from szradm

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+TEMPFILE=$(mktemp)
+
+szradm -q list-global-variables --format=json > $TEMPFILE
+
+
+
+
 cat <<EOCREDS > /scalr-reporting/creds.json
 {
-    "api_url": "$API_URL",
-    "api_key_id": "$API_KEY_ID",
-    "api_key_secret": "$API_KEY_SECRET"
+    "api_url": "$(cat $TEMPFILE | jq -r '.variables.values.API_URL')",
+    "api_key_id": "$(cat $TEMPFILE | jq -r '.variables.values.API_KEY_ID')",
+    "api_key_secret": "$(cat $TEMPFILE | jq -r '.variables.private_values.API_KEY_SECRET')"
 }
 EOCREDS
 
